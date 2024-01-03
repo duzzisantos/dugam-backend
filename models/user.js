4f91404eae4d259fa66d0541c816b15c37103beb
Duzie <duzzi4x@gmail.com>
2024-01-03 22:23:55 +0100

    Refactored user schema as singular source of truth

--- models/user.js
+++ models/user.js
@@ -3,19 +3,59 @@ const Schema = mongoose.Schema;
 
 const UserAccount = new Schema(
   {
-    username: {
+    userId: {
       type: String,
-      required: true,
       unique: true,
     },
-    password: {
+    userEmail: {
       type: String,
-      required: true,
+      unique: true,
     },
-    phone: {
-      type: String,
-      required: true,
-    },
+    registeredBusinesses: [
+      {
+        businessID: String,
+        firstName: String,
+        lastName: String,
+        businessName: String,
+        address: String,
+        email: String,
+        businessPhone: String,
+        category: String,
+        photos: [{ imageId: String, image: String }],
+      },
+    ],
+    ratings: [
+      {
+        ratingsId: String,
+        ratingsContent: String,
+        receivedBy: String,
+        ratingsDate: String,
+      },
+    ],
+    followers: [
+      {
+        followerId: String,
+        followerName: String,
+        isBlocked: Boolean,
+        isUnfollowed: Boolean,
+      },
+    ],
+    following: [
+      {
+        followerId: String,
+        followerName: String,
+        isBlocked: Boolean,
+        isUnfollowed: Boolean,
+      },
+    ],
+    userContent: [
+      {
+        contentId: String,
+        contentBody: String,
+        contentImage: String,
+        likes: [{ likeId: String, isUnliked: Boolean }],
+      },
+    ],
   },
   { timestamps: true }
 );
