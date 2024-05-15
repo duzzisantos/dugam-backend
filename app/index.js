4c6a67d61d42f94c96d3eee268b71afbe5b72346
Duzie <duzzi4x@gmail.com>
2024-05-15 21:05:49 +0200

    testing wihout headers

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { jwtDecode } = require("jwt-decode");
-const RateLimit = require("express-rate-limit");
+// const RateLimit = require("express-rate-limit");
 const db = require("../models");
 const helmet = require("helmet");
 const methodOverride = require("method-override");
@@ -28,20 +28,20 @@ db.mongoose
 
 const corsOptions = {
   origin: "*",
-  methods: "GET POST PUT DELETE",
+  methods: "GET POST PUT DELETE OPTIONS",
 };
 
-const rateLimiter = RateLimit({
-  windowMs: 1 * 60 * 100,
-  max: 50,
-});
+// const rateLimiter = RateLimit({
+//   windowMs: 1 * 60 * 100,
+//   max: 50,
+// });
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride());
 app.use(helmet());
-app.use(rateLimiter);
+// app.use(rateLimiter);
 
 app.use(
   mongoSanitize({
@@ -100,22 +100,22 @@ app.use("/", (err, req, res, next) => {
 });
 
 //MiddleWare for checking authorized users
-app.use((req, res, next) => {
-  const token =
-    req.headers.authorization && req.headers.authorization.split(" ")[1];
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized Access" });
-  }
+// app.use((req, res, next) => {
+//   const token =
+//     req.headers.authorization && req.headers.authorization.split(" ")[1];
+//   if (!token) {
+//     return res.status(401).json({ message: "Unauthorized Access" });
+//   }
 
-  const decodedToken = jwtDecode(token);
+//   const decodedToken = jwtDecode(token);
 
-  if (decodedToken.aud === process.env.REACT_APP_AUTHORIZATION_AUD) {
-    req.decodedToken = decodedToken;
-    next();
-  } else {
-    res.status(401).json({ message: "Unauthorized Access" });
-  }
-});
+//   if (decodedToken.aud === process.env.REACT_APP_AUTHORIZATION_AUD) {
+//     req.decodedToken = decodedToken;
+//     next();
+//   } else {
+//     res.status(401).json({ message: "Unauthorized Access" });
+//   }
+// });
 
 const PORT = 8080;
 app.listen(PORT, (err) => {
