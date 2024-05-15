daea06817648d8d6f3860727b5946079da5070f3
Duzie <duzzi4x@gmail.com>
2024-05-15 18:57:18 +0200

    improvement

--- app/index.js
+++ app/index.js
@@ -39,18 +39,6 @@ const rateLimiter = RateLimit({
 });
 
 app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Headers, *, Access-Control-Allow-Origin",
-    "Origin, X-Requested-with, Content_Type,Accept,Authorization",
-    "http://localhost:3000"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
-    return res.status(200).json({});
-  }
-  next();
-});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride());
